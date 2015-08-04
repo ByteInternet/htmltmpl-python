b9d36ab785627fb0414879a081c115c970e2c775
Allard Hoeve <allard@byte.nl>
2015-08-04 15:09:48 +0200

    Move to setup from setuptools to enable wheel

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name = "htmltmpl",
       version = "1.22",
